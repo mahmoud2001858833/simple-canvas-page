2d25d3f3bd7a269b78dcdd64971adb27aae48e0f
mahmoud <jowmahmoud6@gmail.com>
2026-04-26 19:20:39 +0300

    تم تبديل الملفات بملفات المشروع الجديد

--- src/pages/Index.tsx
+++
@@ -1,9 +0,0 @@
-const Index = () => {
-  return (
-    <div className="min-h-screen bg-background flex items-center justify-center">
-      <h1 className="text-2xl font-light text-foreground">مرحباً</h1>
-    </div>
-  );
-};
-
-export default Index;
